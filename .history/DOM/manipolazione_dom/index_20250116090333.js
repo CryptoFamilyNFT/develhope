4701bfd508afc94b31ff6618f88b7056b37c3df0
CryptoFamilyNFT <95889492+CryptoFamilyNFT@users.noreply.github.com>
2025-01-16 09:34:53 +0100

    feat: added DOM/manipolazione_dom

--- .history/DOM/manipolazione_dom/index_20250116090333.js
+++
@@ -1,7 +0,0 @@
-const addProduct = () => {
-  () => {
-    const product = document.createElement("li");
-    product.textContent = "Product";
-    document.querySelector("ul").appendChild(product);
-  }
-};
--- .history/DOM/manipolazione_dom/index_20250116090849.js
+++
@@ -1,8 +0,0 @@
-const addProduct = () => {
-    const $ul = document.querySelector("ul");
-    const product = document.createElement("li");
-    const $inputElement = document.querySelector("input");
-    product.textContent = $inputElement.value;
-    const newCheckbox = document.createElement("input-checkbox").innerHTML = `<input type="checkbox">${product}</input>`;
-    newCheckbox.appendChild(product);
-};
